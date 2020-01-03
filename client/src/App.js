0b7cb31bb767280dc96e6aacbdbad69ba72462e2
Tommy Vinyard <tpvinyard@gmail.com>
2020-01-02 22:16:51 -0600

    add client routes, basic Nav component, basic pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Saved from "./pages/Saved";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="App">
-      Made it!
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/books" component={Books} />
+          <Route exact path="/saved" component={Saved} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
