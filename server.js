3554604d9217a6f0179d4e2588802116e21bf717
Tommy Vinyard <tpvinyard@gmail.com>
2020-01-02 21:15:30 -0600

    adding routes

--- server.js
+++ server.js
@@ -12,12 +12,10 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
